96bee05e2923d4b1fe4ef19ce9ceaed1fd50cedc
Levi Hackwith <levi.hackwith@gmail.com>
2012-05-31 21:30:03 -0500

    Add ability to get user repos list, future-proofing
    * Added `getUserRepos` method
    * Added `authenticateUser` method (untested)
    * Added `getCommandMethod` method to make sending specific HTTP verbs with a request simpler 
      (currently supports GET and POST)
    * `callAPI` method now sends request via GET by default

--- GitJs.js
+++ GitJs.js
@@ -1,16 +1,49 @@
 function GitJs(config) {
     'use strict';
     var defaults = {
+        inheriting: false,
+        clientId: undefined,
+        accessToken: undefined,
         baseUrl: 'https://api.github.com',
-        mode: 'read'
+        mode: 'read',
+        apiErrorHandler: function () {},
+        apiSuccessHandler: function () {}
+    },
+    init = function () {
+        switch(this.config.mode) {
+            case 'read':
+                break;
+            case 'write':
+                break;
+            default:
+                throw this.config.mode + " is not a valid 'mode' value. Accepted values are 'read' and 'write'.";
+        }
     };
     $.extend(config, defaults);
     this.config = config;
+    if(this.config.inheriting == false) {
+        init();
+    }
 }
 
-GitJs.prototype.callApi = function (apiCommand, data) {
+GitJs.prototype.getCommandMethod = function (httpVerb) {
+    var method = $.get;
+    switch(httpVerb) {
+        case 'GET':
+            method = $.get;
+            break;
+        case 'POST':
+            method = $.post;
+            break;
+    }
+    return method;
+}
+
+GitJs.prototype.callApi = function (apiCommand, data, httpVerb) {
     'use strict';
-    data = data || {};
+
+    var commandMethod = this.getCommandMethod(httpVerb || 'GET'),
+        me = this;
 
     if (apiCommand[0] !== '/') {
         apiCommand = '/' + apiCommand;
@@ -18,15 +51,24 @@ GitJs.prototype.callApi = function (apiCommand, data) {
 
     return {
         url: 'https://api.github.com' + apiCommand,
-        data: data,
+        data: data || {},
         send: function (callback) {
-            $.get(this.url, this.data, callback);
+            commandMethod.call(me, this.url, this.data, callback, 'json');
         }
     };
 };
 
+GitJs.prototype.authenticateUser = function (accessToken) {
+    accessToken = accessToken || this.config.accessToken;
+    this.config.accessToken = accessToken;
+    this.callApi('/', {
+        access_token: this.accessToken
+    });
+}
+
 GitJs.prototype.getGistComments = function (gistId, commentId, callback) {
     'use strict';
+
     var apiCommand = '';
     if (commentId === undefined || isNaN(commentId) === true) {
         apiCommand = '/gists/' + gistId + '/comments';
@@ -35,3 +77,15 @@ GitJs.prototype.getGistComments = function (gistId, commentId, callback) {
     }
     this.callApi(apiCommand).send(callback);
 };
+
+GitJs.prototype.getUserRepos = function(username, type, callback) {
+    var apiCommand = '';
+    if(username !== undefined) {
+        apiCommand = '/users/' + username + '/repos';
+    } else {
+        apiCommand = '/users/repos';
+    }
+    this.callApi(apiCommand, {
+        type: type || 'all'
+    }).send(callback);
+}
