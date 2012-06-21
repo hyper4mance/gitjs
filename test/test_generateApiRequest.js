202854b4b36fb1f38f67f364f63972ee563553f4
Levi Hackwith <levi.hackwith@gmail.com>
2012-06-20 22:08:21 -0500

    Stub $.create method in generateApiRequest unit test
    This closes #58.

--- test/test_generateApiRequest.js
+++ test/test_generateApiRequest.js
@@ -5,19 +5,49 @@
 
 TestCase("generateApiRequestTest", {
     setUp: function () {
+        this.$get = $.get;
+        this.$post = $.post;
+        $.get = function() {
+            $.get.called = true;
+        }        
         this.gitjs = new GitJs();
     },
     tearDown: function () {
-        delete this.gitJs;
+        $.get = this.$get;
+        $.post = this.$post;
+        
+        delete this.gitjs;
+        delete this.$get;
+        delete this.$post;
     },
 
     "test generateApiRequest with minimal options": function () {
         var apiCommand = '/user/opnsrce/repos',
-            apiRequest = this.generateApiRequest(apiCommand);
+            apiRequest = this.gitjs.generateApiRequest(apiCommand);
             
-        this.assertTypeOf('function' , this.gitjs.generateApiRequest);
-        this.assertUndefined(apiRequest.data);
-        this.assertEquals('https://api.github.com' + apiCommand, apiRequest.url);
-        this.assertTypeOf('function', apiRequest.send);
-    }
+        assertTypeOf('object', apiRequest.data);
+        assertEquals(0, Object.keys(apiRequest.data).length);
+        assertEquals('https://api.github.com' + apiCommand, apiRequest.url);
+        assertTypeOf('function', apiRequest.send);
+        assertEquals('GET', apiRequest.httpVerb);
+        assertEquals('jsonp', apiRequest.dataType);
+    },
+    "test generateApiRequest with all options": function () {
+        var apiCommand = '/user/opnsrce/repos',
+            testData = {
+                username: 'opnsrce'
+            },
+            httpVerb = 'POST',
+            dataType = 'json',
+            apiRequest = this.gitjs.generateApiRequest(apiCommand, testData, httpVerb, dataType);
+            
+        apiRequest.send();
+        
+        assertEquals(testData, apiRequest.data);
+        assertEquals('https://api.github.com' + apiCommand, apiRequest.url);
+        assertTypeOf('function', apiRequest.send);
+        assertEquals(httpVerb, apiRequest.httpVerb);
+        assertEquals(dataType, apiRequest.dataType);
+        assertTrue($.get.called);
+    },
 });
