e4dda6f081e1c11bab6949b49ea60f97e9b22b7e
Levi Hackwith <levi.hackwith@gmail.com>
2012-06-23 16:36:27 -0500

    Add unit test for getReposByOrg method using minimal options

--- test/test_getReposByOrgMethod.js
+++ test/test_getReposByOrgMethod.js
@@ -3,20 +3,39 @@
 
 'use strict';
 
-AsyncTestCase("getReposByOrg", {
+TestCase("getReposByOrg", {
     setUp: function () {
+        var me = this;
+           
         this.gitjs = new GitJs();
+        this.generateApiRequestOriginal = this.gitjs.generateApiRequest
+        this.gitjs.generateApiRequest = function (apiCommand, data, httpVerb, dataType) {
+            var request = me.generateApiRequestOriginal.apply(new GitJs(), arguments);
+            request.send = function() {
+                me.gitjs.sendApiRequestCalled = true;
+            }
+            me.apiRequest = request;
+            return request;
+        }        
     },
     tearDown: function () {
-        delete this.gitJs;
+        this.gitjs.generateApiRequest = this.generateApiRequestOriginal;
+        
+        delete this.generateApiRequestOriginal;
+        delete this.gitjs;
+        delete this.apiRequest;
     },
 
+
     "test getReposByOrg method with minimal options": function () {
-        var organization = '',
-            callback = function(data, textStatus, jqXhr) {
-                assertTrue(data.length > 0);
-                assertEquals(textStatus, '4500');    
-            };
-        this.gitjs.getReposByOrg(callback, organization);
+        this.gitjs.getReposByOrg(function(){}, 'org');
+        assertTypeOf('object', this.apiRequest);
+        assertTrue(this.gitjs.sendApiRequestCalled);
+        assertEquals('https://api.github.com/orgs/org/repos', this.apiRequest.url);
+        assertEquals('jsonp', this.apiRequest.dataType);
+        assertEquals('GET', this.apiRequest.httpVerb);
+        assertUndefined(this.apiRequest.data.sort);
+        assertUndefined(this.apiRequest.data.type);
+        assertUndefined(this.apiRequest.data.direction);
     }
 });
