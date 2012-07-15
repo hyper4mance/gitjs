c4bae3439b7ff31ce79c075a123f3f0395d1146b
Levi Hackwith <levi.hackwith@gmail.com>
2012-07-15 13:08:48 -0500

    Set up example for how to use userAuthenticate method
    I set up the basic layout for the example page on how to use the
    authenticateUser method (see #46). Right now it just pulls in the
    user's profile pic as well as there name, email and website. There's
    still more that needs to be done in terms of polish, but I feel it's a
    good start and a good foundation to build on.

---
+++ examples/authentication/assets/js/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    'use strict';
+    $('form').bind('submit', function () {
+        var gitJs = new GitJs({
+            accessToken: $('input[name=access_token]').val()
+        });
+        gitJs.authenticateUser(function (data, text, jqXhr) {
+            showUserInfo(data);
+        });
+        return false;
+    });
+});
+
+function showUserInfo(userObject) {
+    'use strict';
+    var userInfo = $('#user_info'),
+        username = userInfo.find('#username'),
+        avatar = userInfo.find('#avatar'),
+        email = userInfo.find('#email'),
+        blog = userInfo.find('#blog'),
+        userData = userObject.data;
+
+    avatar.attr('src', 'http://www.gravatar.com/avatar/' + userData.gravatar_id + '?s=260');
+    email.html(userData.email);
+    blog.html(userData.blog);
+    username.html(userData.login);
+}
