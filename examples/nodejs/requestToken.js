a7d6a04772b8d1a874fad9aef6ecf69f14a30568
Levi Hackwith <levi.hackwith@gmail.com>
2012-07-16 21:06:10 -0500

    Remove requestToken.js
    File is unused.

--- examples/nodejs/requestToken.js
+++
@@ -1,6 +0,0 @@
-var http = require('http'),
-    url = require('url'),
-    clientId = '9161914a06ffaf898a7e',
-    appSecret = '22689d87189698f4bbf05c90585b9ff7bd8602e7',
-    accessToken = '',
-    temporaryCode = '';
