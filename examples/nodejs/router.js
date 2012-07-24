c0b54e1ff568cced1ae02bac1e751b1650260eb2
Levi Hackwith <levi.hackwith@gmail.com>
2012-07-23 22:24:12 -0500

    Make web server server html files

--- examples/nodejs/router.js
+++ examples/nodejs/router.js
@@ -1,16 +1,21 @@
 function route(handle, pathname, request, response) {
     'use strict';
+    var fs = require('fs'),
+        html;
     console.log("About to route a request for " + pathname);
+
     if (typeof handle[pathname] === 'function') {
         handle[pathname](request, response);
     } else {
-        console.log("No request handler found for " + pathname);
-        response.writeHead(404, {
-            "Content-Type": "text/plain"
-        });
-        response.write("404 Not found");
-        response.end();
+        try {
+            html = fs.readFileSync('./' + pathname, 'ascii');
+            response.write(html);
+            response.end();
+        } catch (err) {
+            response.write(err);
+        }
     }
+    response.end();
 }
 
 exports.route = route;
