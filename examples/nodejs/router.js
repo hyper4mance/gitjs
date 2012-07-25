941bf406d3d2b94e16db2965837c890d1390f28a
Levi Hackwith <levi.hackwith@gmail.com>
2012-07-24 23:56:03 -0500

    Fix issue where Node server crashes after first page load
    The server was borking when it got a request for the favicon. I wrapped
    the logic for reading a file in an if-statement that bypasses the
    function for that path.

--- examples/nodejs/router.js
+++ examples/nodejs/router.js
@@ -2,16 +2,19 @@ function route(handle, pathname, request, response) {
     'use strict';
     var fs = require('fs'),
         html;
-        
+
     if (typeof handle[pathname] === 'function') {
         handle[pathname](request, response);
     } else {
-        try {
-            html = fs.readFileSync('./' + pathname, 'ascii');
-            response.write(html);
-            response.end();
-        } catch (err) {
-            response.write(err);
+        if(pathname != '/favicon.ico') {
+            try {
+                console.log(pathname);
+                html = fs.readFileSync('./' + pathname, 'ascii');
+                response.write(html);
+                response.end();
+            } catch (err) {
+                console.log(err);
+            }
         }
     }
     response.end();
