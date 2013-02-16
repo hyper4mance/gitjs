d09c500d0dd0deb195a742dacb8ea30cdce12b54
Levi Hackwith <levi.hackwith@gmail.com>
2013-02-16 12:58:56 -0600

    Switch from single-quotes to double-quotes

--- test/browser/test_generateAuthorizationLink.js
+++ test/browser/test_generateAuthorizationLink.js
@@ -4,7 +4,7 @@
     'use strict';
     var assert = buster.assert;
 
-    buster.testCase("generateAuthorizationLinksTest", {
+    buster.testCase('generateAuthorizationLinksTest', {
         setUp: function () {
             this.gitjs = new GitJs();
         },
@@ -12,11 +12,11 @@
             delete this.gitJs;
         },
 
-        "test generateAuthorizationLink method with minimal options": function () {
+        'test generateAuthorizationLink method with minimal options': function () {
             var expectedResult = 'https://github.com/login/oauth/authorize?client_id=123';
             assert.equals(this.gitjs.generateAuthorizationLink('123'), expectedResult);
         },
-        "test generateAuthorizationLink method with all options": function () {
+        'test generateAuthorizationLink method with all options': function () {
             var expectedResult = 'https://github.com/login/oauth/authorize?client_id=123&scope=public&redirect_uri=http://www.cnn.com';
             assert.equals(this.gitjs.generateAuthorizationLink('123', {scope: 'public', redirectUri: 'http://www.cnn.com'}), expectedResult);
         }
