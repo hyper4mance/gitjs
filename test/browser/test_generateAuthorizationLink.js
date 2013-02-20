fb5cf3708b097b04a768dd4ef9a8d1f6d1bad00d
Levi Hackwith <levi.hackwith@gmail.com>
2013-02-19 20:42:07 -0600

    Reformat generateAuthorizationLink test

--- test/browser/test_generateAuthorizationLink.js
+++ test/browser/test_generateAuthorizationLink.js
@@ -13,14 +13,14 @@
     });
 
     test('test generateAuthorizationLink method with minimal options', function () {
-            var expectedResult = 'https://github.com/login/oauth/authorize?client_id=123';
+        var expectedResult = 'https://github.com/login/oauth/authorize?client_id=123';
 
-            strictEqual(this.gitJs.generateAuthorizationLink('123'), expectedResult);
+        strictEqual(this.gitJs.generateAuthorizationLink('123'), expectedResult);
     });
 
     test('test generateAuthorizationLink method with all options', function () {
-            var expectedResult = 'https://github.com/login/oauth/authorize?client_id=123&scope=public&redirect_uri=http://www.cnn.com';
+        var expectedResult = 'https://github.com/login/oauth/authorize?client_id=123&scope=public&redirect_uri=http://www.cnn.com';
 
-            strictEqual(this.gitJs.generateAuthorizationLink('123', {scope: 'public', redirectUri: 'http://www.cnn.com'}), expectedResult);
+        strictEqual(this.gitJs.generateAuthorizationLink('123', {scope: 'public', redirectUri: 'http://www.cnn.com'}), expectedResult);
     });
 }(GitJs));
