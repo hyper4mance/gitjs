41a32b9e86a6004dd5b8115f852c6dfab05662e6
Levi Hackwith <levi.hackwith@gmail.com>
2013-02-20 22:02:44 -0600

    Switch from single quotes to double quotes

--- test/browser/test_getReposByOrg.js
+++ test/browser/test_getReposByOrg.js
@@ -6,38 +6,38 @@
     module('getReposByOrg Tests', {
         setup: function() {
             var serverResponse = [{
-                "id": 1296269,
-                "owner": {
-                    "login": "octocat",
-                    "id": 1,
-                    "avatar_url": "https://github.com/images/error/octocat_happy.gif",
-                    "gravatar_id": "somehexcode",
-                    "url": "https://api.github.com/users/octocat"
+                'id': 1296269,
+                'owner': {
+                    'login': 'octocat',
+                    'id': 1,
+                    'avatar_url': 'https://github.com/images/error/octocat_happy.gif',
+                    'gravatar_id': 'somehexcode',
+                    'url': 'https://api.github.com/users/octocat'
                 },
-                "name": "Hello-World",
-                "full_name": "octocat/Hello-World",
-                "description": "This your first repo!",
-                "private": false,
-                "fork": false,
-                "url": "https://api.github.com/repos/octocat/Hello-World",
-                "html_url": "https://github.com/octocat/Hello-World",
-                "clone_url": "https://github.com/octocat/Hello-World.git",
-                "git_url": "git://github.com/octocat/Hello-World.git",
-                "ssh_url": "git@github.com:octocat/Hello-World.git",
-                "svn_url": "https://svn.github.com/octocat/Hello-World",
-                "mirror_url": "git://git.example.com/octocat/Hello-World",
-                "homepage": "https://github.com",
-                "language": null,
-                "forks": 9,
-                "forks_count": 9,
-                "watchers": 80,
-                "watchers_count": 80,
-                "size": 108,
-                "master_branch": "master",
-                "open_issues": 0,
-                "pushed_at": "2011-01-26T19:06:43Z",
-                "created_at": "2011-01-26T19:01:12Z",
-                "updated_at": "2011-01-26T19:14:43Z"
+                'name': 'Hello-World',
+                'full_name': 'octocat/Hello-World',
+                'description': 'This your first repo!',
+                'private': false,
+                'fork': false,
+                'url': 'https://api.github.com/repos/octocat/Hello-World',
+                'html_url': 'https://github.com/octocat/Hello-World',
+                'clone_url': 'https://github.com/octocat/Hello-World.git',
+                'git_url': 'git://github.com/octocat/Hello-World.git',
+                'ssh_url': 'git@github.com:octocat/Hello-World.git',
+                'svn_url': 'https://svn.github.com/octocat/Hello-World',
+                'mirror_url': 'git://git.example.com/octocat/Hello-World',
+                'homepage': 'https://github.com',
+                'language': null,
+                'forks': 9,
+                'forks_count': 9,
+                'watchers': 80,
+                'watchers_count': 80,
+                'size': 108,
+                'master_branch': 'master',
+                'open_issues': 0,
+                'pushed_at': '2011-01-26T19:06:43Z',
+                'created_at': '2011-01-26T19:01:12Z',
+                'updated_at': '2011-01-26T19:14:43Z'
             }];
 
             this.server = sinon.fakeServer.create();
@@ -56,7 +56,7 @@
         }
     });
 
-    test("test that callback gets called when called with minimal options", function () {
+    test('test that callback gets called when called with minimal options', function () {
         var callback = this.spy();
 
         this.gitJs.getReposByOrg(callback, 'octocat-org');
@@ -65,7 +65,7 @@
         ok(callback.called);
     });
 
-    test("test that callback gets called when called with all options", function () {
+    test('test that callback gets called when called with all options', function () {
         var callback = this.spy();
 
         this.gitJs.getReposByOrg(callback, 'octocat-org', {
@@ -73,7 +73,7 @@
             sort: 'created',
             direction: 'asc'
         });
-        
+
         this.server.respond();
 
         ok(callback.called);
